a8c4c9271c37e17bd48877b20e316741864d2747
yikezawa <y.ikezawa93@gmail.com>
2020-09-25 00:15:43 +0900

    mod

---
+++ src/bin/err_panic.rs
@@ -0,0 +1,24 @@
+use anyhow::{Context, Result};
+
+enum MyError{
+    Io(std::io::Error),
+    Num(std::num::ParseIntError),
+}
+
+fn get_int_from_file() -> Result<i32>{
+    let path = "number.txt";
+    let num_str = std::fs::read_to_string(path).with_context(|| format!("failed to read string from {}", path))?;
+
+    num_str
+        .trim()
+        .parse::<i32>()
+        .map(|t| t*2)
+        .context("failed to parse string")
+}
+
+fn main(){
+    match get_int_from_file(){
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e),
+    }
+}
