ec261df19a81aa24f335ccc71a53e92f1a3491d4
yikezawa <y.ikezawa93@gmail.com>
2020-09-25 23:15:08 +0900

    mod

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use anyhow::{bail, ensure,Result, Context};
 use clap::Clap;
 use std::fs::File;
 use std::io::{stdin, BufRead,BufReader};
+use std::path::PathBuf;
 
 struct RpnCalculator(bool);
 
@@ -60,7 +61,7 @@ struct Opts {
     #[clap(short, long)]
     verbose: bool,
     #[clap(name="FILE")]
-    formula_file: Option<String>,
+    formula_file: Option<PathBuf>,
 }
 
 fn main() -> Result<()>{
